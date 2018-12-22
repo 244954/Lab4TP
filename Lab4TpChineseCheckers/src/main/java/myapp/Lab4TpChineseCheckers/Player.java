4df5512d88eb76a171a1f7c78b63b4353a59c67a
TeeKay18 <tommkar321@wp.pl>
2018-12-22 17:06:55 +0100

    More self-commenting code

--- Lab4TpChineseCheckers/src/main/java/myapp/Lab4TpChineseCheckers/Player.java
+++ Lab4TpChineseCheckers/src/main/java/myapp/Lab4TpChineseCheckers/Player.java
@@ -4,16 +4,15 @@ import java.util.List;
 
 public abstract class Player extends Thread{
 
-	int noPlayer;
-	List<Player> opponents;
+    int noPlayer;
+    List<Player> opponents;
     Game game;
     int pawnx,pawny;
     protected volatile boolean current;
     protected boolean pawnLocked;
 	
     abstract public int getnoPlayer();
-    
-	abstract public void otherPlayerMoved(int x,int y,int nx,int ny);
-	abstract public void setOpponents(List<Player> p);
-	abstract public void setCurrent(boolean b);
+    abstract public void otherPlayerMoved(int x,int y,int nx,int ny);
+    abstract public void setOpponents(List<Player> p);
+    abstract public void setCurrent(boolean b);
 }
